93240f58e77e7e4f4464ddf214c1c10e7206a407
paguerre3 <camila.milagros.aguerre@gmail.com>
2024-12-05 01:44:26 -0300

    ehckmark verification

--- internal/infrastructure/0_registration_client_test.go
+++ internal/infrastructure/0_registration_client_test.go
@@ -9,7 +9,8 @@ import (
 
 func TestRegister(t *testing.T) {
 	//response, statusCode, err := handler.Register("DarioAguerre", "ARG", "aguerrepablodario@gmail.com", "engineering")
-	response, statusCode, err := handler.Register("aAguerre Pablo D.", "ARG", "aguerrepablodario@hotmail.com", "engineering")
+	//response, statusCode, err := handler.Register("aAguerre Pablo D.", "ARG", "aguerrepablodario@hotmail.com", "engineering")
+	response, statusCode, err := handler.Register("PabloDariAguerre", "ARG", "p_d_aguerre@outlook.com", "engineering")
 
 	assert.NoError(t, err)
 	assert.Equal(t, 200, statusCode)
@@ -18,6 +19,7 @@ func TestRegister(t *testing.T) {
 	/** e-mail received:
 	Esta es tu API-KEY para la AltScore Contest
 	//255292ff68394c6eb6136069a034bf28
-	36b39441ba7040fea158db6a4103aaa6
+	//36b39441ba7040fea158db6a4103aaa6
+	20adafe2bf3a4a43b20792aceac3dad9
 	*/
 }
