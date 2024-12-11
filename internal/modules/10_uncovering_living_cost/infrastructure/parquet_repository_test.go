fd4bd8431023ff400a53f2b34a328fc13c1b2f18
paguerre3 <camila.milagros.aguerre@gmail.com>
2024-12-11 15:19:50 -0300

    test valid key filtering

---
+++ internal/modules/10_uncovering_living_cost/infrastructure/parquet_repository_test.go
@@ -0,0 +1,38 @@
+package infrastructure
+
+import (
+	"testing"
+
+	"github.com/hashicorp/go-set/v3"
+	app "github.com/paguerre3/as/internal/modules/10_uncovering_living_cost/application"
+	domain "github.com/paguerre3/as/internal/modules/10_uncovering_living_cost/domain"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIsValidKey(t *testing.T) {
+	h3TrainKeys := set.New[string](100)
+	h3TrainKeys.Insert("8966d3285c3ffff")
+	h3TestKeys := set.New[string](100)
+	h3TestKeys.Insert("8966d3285c3ffff")
+	h3TestKeys.Insert("9966d3285c3ffff")
+	h3Keys := h3TrainKeys.Union(h3TestKeys)
+	uc := app.NewH3UseCase()
+	pr := &parquetRepositoryImpl{
+		offset:    0,
+		h3Keys:    h3Keys,
+		h3UseCase: uc,
+	}
+	assert.True(t, true)
+	md := new(domain.MobilityData)
+	md.Lat = -0.313305
+	md.Lon = -78.536396
+	assert.True(t, pr.validH3Key(*md))
+	md = new(domain.MobilityData)
+	md.Lat = -0.31339
+	md.Lon = -78.536236
+	assert.True(t, pr.validH3Key(*md))
+	md = new(domain.MobilityData)
+	md.Lat = -0.21349
+	md.Lon = -58.536236
+	assert.False(t, pr.validH3Key(*md))
+}
